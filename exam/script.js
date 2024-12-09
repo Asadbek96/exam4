b81b3fc9b202f6de015640a872c83f8c0b3497a5
Asadbek <asadbekesemuratov2@gmail.com>
2024-12-09 19:49:55 +0500

    exam

--- exam/script.js
+++ exam/script.js
@@ -1,47 +1,60 @@
-// TASK-A
-let month = +prompt('1-ден 12-ге дейін сан енгізіңіз:')
+//TASK-1
 
-if (month == 3, 4, 5) {
-  alert('Айы:' + month + '\n' + 'Мезгілі: Көктем')
-} else if (month == 6, 7, 8) {
-  alert('Айы:' + month + '\n' + 'Мезгілі: Жаз')
-} else if (month == 9, 10, 11) {
-  alert('Айы:' + month + '\n' + 'Мезгілі: Күз')
-} else if (month == 12, 1, 2) {
-  alert('Айы:' + month + '\n' + 'Мезгілі: Қыс')
-} else{
-  alert('"1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз')
+let school = {
+  name: 'AmjiltCyberSchool',
+  phoneNumber: +777707070707,
+  students: [
+    {
+      name: 'Asadbek',
+      top: '24/8',
+      progress: 100,
+      lesson: 2,
+    },
+    {
+      name: 'Meir',
+      top: '24/8',
+      progress: 90,
+      lesson: 2,
+    },
+    {
+      name: 'Asan',
+      top: '24/3',
+      progress: 70,
+      lesson: 2,
+    },
+    {
+      name: 'Usen',
+      top: '24/5',
+      progress: 90,
+      lesson: 2,
+    },
+    {
+      name: 'Omar',
+      top: '24/1',
+      progress: 60,
+      lesson: 2,
+    },
+  ],
+  //TASK-2
+  changeSchoolName: () => {
+    let newschoolName = prompt('Jana mekteptin atyn engiziniz')
+    newschoolName = school.name
+    console.log(school.name)
+  },
+  //TASK-3
+  changePhoneNumber: () => {
+    let newPhoneNumber = prompt('Jana mekteptin atyn engiziniz')
+    newPhoneNumber = school.phoneNumber
+    if (newPhoneNumber == ' ') {
+      return console.log(school.phoneNumber)
+    } else {
+      console.log(newPhoneNumber)
+    }
+  },
+  //TASK-4
+  listStudentNames: () => {   
+    return this.students.name
+  },
 }
+console.log(school);
 
-//TASK-B
-let a = +prompt('Бірінші санды енгізіңіз:')
-let b = +prompt('Екінші санды енгізіңіз:')
-
-switch(a,b){      
-    case a>=b:
-        alert(' Үлкен сан' + a && ('Орташа мән'(a + b) / 2))
-    break;
-    case b>=a:
-        alert(' Үлкен сан' + b && ('Орташа мән'(a + b) / 2))
-    break;
-    default:
-    alert('Error')
-}
-
-//TASK-C
-let a = +prompt('Бірінші санды енгізіңіз:')
-let b = +prompt('Екінші санды енгізіңіз:')
-let с = +prompt('Үшінші санды енгізіңіз:')
-
-if (a >= b || a >= c) {
-  alert('Үлкен сан:' + a && ('Кіші сан:' + b || 'Кіші сан:' + c) && (a - b - c))
-}
-else if (b >= a || b >= c) {
-  alert('Үлкен сан:' + b && ('Кіші сан:' + c || 'Кіші сан:' + a) && (a - b - c))
-}
-else if (c >= a || c >= b) {
-  alert('Үлкен сан:' + c && ('Кіші сан:' + a || 'Кіші сан:' + b) && (a - b - c))
-}
-else{
-    alert('Error')
-}
